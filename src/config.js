260c727e3acfd2aa2cb237a61ba4ce5861c887f9
eddie <eddiekim.dev.pro@gmail.com>
2018-12-07 09:30:17 +1300

    update

--- src/config.js
+++ src/config.js
@@ -69,8 +69,8 @@ export default {
 
     views: [
         // {viewName: 'Day', viewType: ViewTypes.Day, showAgenda: false, isEventPerspective: false},
-        {viewName: 'Week', viewType: ViewTypes.Week, showAgenda: false, isEventPerspective: false},
-        {viewName: 'Month', viewType: ViewTypes.Month, showAgenda: false, isEventPerspective: false},
+        // {viewName: 'Week', viewType: ViewTypes.Week, showAgenda: false, isEventPerspective: false},
+        // {viewName: 'Month', viewType: ViewTypes.Month, showAgenda: false, isEventPerspective: false},
         // {viewName: 'Quarter', viewType: ViewTypes.Quarter, showAgenda: false, isEventPerspective: false},
         // {viewName: 'Year', viewType: ViewTypes.Year, showAgenda: false, isEventPerspective: false},
     ],
